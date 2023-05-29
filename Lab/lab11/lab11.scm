fe5d882c62d99cffc6b0a24182bcd882d94af420
yifu <77519205+yifu-deng@users.noreply.github.com>
2023-05-29 14:41:21 -0400

    over-or-under

--- Lab/lab11/lab11.scm
+++ Lab/lab11/lab11.scm
@@ -1,8 +1,8 @@
 (define (over-or-under num1 num2) 
   (cond
     ((< num1 num2) -1)
-    ((> num1 num2) 0)
-    (else 0))
+    ((= num1 num2) 0)
+    (else 1))
 )
 
 (define (make-adder num) 'YOUR-CODE-HERE)
