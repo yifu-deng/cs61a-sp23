9fdf3f53090c4b95668589aaa67d100373155922
yifu <77519205+yifu-deng@users.noreply.github.com>
2023-05-29 14:56:18 -0400

    lab11

--- Lab/lab11/lab11.scm
+++ Lab/lab11/lab11.scm
@@ -5,11 +5,20 @@
     (else 1))
 )
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (inc) (+ inc num))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+  ; note: this relies on `composed` being implemented correctly
+  (if (< n 1)
+    (lambda (x) x)
+    (composed f (repeat f (- n 1))))
+)
 
 (define (max a b)
   (if (> a b)
@@ -21,4 +30,11 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (max a b) (if (> a b) a b))
+(define (min a b) (if (> a b) b a))
+(define (gcd a b)
+  (cond ((zero? a) b)
+        ((zero? b) a)
+        ((= (modulo (max a b) (min a b)) 0) (min a b))
+        (else (gcd (min a b) (modulo (max a b) (min a b)))))
+)
--- Lab/lab11/tests/combinations.py
+++ Lab/lab11/tests/combinations.py
@@ -7,97 +7,75 @@ test = {
         {
           'code': r"""
           scm> (- 10 4)
-          71dc1c0558e41b2d6d30fd9795b4fb1f
-          # locked
+          6
           scm> (* 7 6)
-          afa373c99abc5a519735aa7466635f88
-          # locked
+          42
           scm> (+ 1 2 3 4)
-          994cc386b9ac20ceafc06b188d1ee65b
-          # locked
+          10
           scm> (/ 8 2 2)
-          805a87056a1a3fd559e4ef12a815b2be
-          # locked
+          2
           scm> (quotient 29 5)
-          9934e055a74f1f7f5fb94c0f9fd6402d
-          # locked
+          5
           scm> (modulo 29 5)
-          3cfd97a152be55d1a3486dbacb2bf637
-          # locked
+          4
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (= 1 3)                    ; Scheme uses '=' instead of '==' for comparison
-          6687e91ee1a92d6553457317c0165f4a
-          # locked
+          #f
           scm> (< 1 3)
-          811dacf2a8c491d71193b389f2e6d3ab
-          # locked
+          #t
           scm> (or 1 #t)                  ; or special form short circuits
-          94ce22b5936436a75abf185df37ba826
-          # locked
+          1
           scm> (and #t #f (/ 1 0))
-          6687e91ee1a92d6553457317c0165f4a
-          # locked
+          #f
           scm> (not #t)
-          6687e91ee1a92d6553457317c0165f4a
-          # locked
+          #f
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (define x 3)
-          5ce45267887fa5dae1771a9b64b54929
-          # locked
+          x
           scm> x
-          350815b30c2ebeb01da1870d87346e85
-          # locked
+          3
           scm> (define y (+ x 4))
-          847f7c178da2025ec82e39b01a424bfd
-          # locked
+          y
           scm> y
-          3f8f8f09d1f65fa9740c33b3c16d4731
-          # locked
+          7
           scm> (define x (lambda (y) (* y 2)))
-          5ce45267887fa5dae1771a9b64b54929
-          # locked
+          x
           scm> (x y)
-          2ac3553c8fa906a0ec29e6b3fe8cf4ea
-          # locked
+          14
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         },
         {
           'code': r"""
           scm> (if (not (print 1)) (print 2) (print 3))
-          94ce22b5936436a75abf185df37ba826
-          350815b30c2ebeb01da1870d87346e85
-          # locked
+          1
+          3
           scm> (* (if (> 3 2) 1 2) (+ 4 5))
-          b1cf566efb20d5b4e8e0fcba34deeb5f
-          # locked
+          9
           scm> (define foo (lambda (x y z) (if x y z)))
-          603330d515a0d452dda52d8a7f3d1dad
-          # locked
+          foo
           scm> (foo 1 2 (print 'hi))
-          2371fa29af94abdb618fc78229de00d9
-          805a87056a1a3fd559e4ef12a815b2be
-          # locked
+          hi
+          2
           scm> ((lambda (a) (print 'a)) 100)
-          a1068219c1264e3c30f090f447a18f5c
-          # locked
+          a
           """,
           'hidden': False,
-          'locked': True,
+          'locked': False,
           'multiline': False
         }
       ],
