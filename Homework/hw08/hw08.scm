3d680efc426544356998490aa3cf835ef069ce59
yifu <77519205+yifu-deng@users.noreply.github.com>
2023-05-29 18:16:07 -0400

    pow method

--- Homework/hw08/hw08.scm
+++ Homework/hw08/hw08.scm
@@ -1,11 +1,16 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp) 
+  (cond ((= exp 0) 1)
+        ((even? exp) (square (pow base (/ exp 2))))
+        (else (* base (pow base (- exp 1)))))
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________________)
+      (let 
+      ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
